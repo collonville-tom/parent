7ae003e679d62b3d03ccfd3a7ccc2efa0fd00de7
Collonville Thomas <collonville_tom@yahoo.fr>
2019-08-05 22:34:36 +0200

    migration settings et travis et jenkinsfile

---
+++ buildOnDockerWithUnix.sh
@@ -0,0 +1,2 @@
+docker pull collonvtom/maven-osgi-dev:3.6.1-jdk-11-slim
+docker run -it --rm -v repomaven:/repolocal -v $(pwd):/maven_work_dir --name mvn collonvtom/maven-osgi-dev:3.6.1-jdk-11-slim
--- buildUnix.sh
+++
@@ -1,2 +0,0 @@
-docker pull collonvtom/maven:latest
-docker run -it --rm -v repomaven:/repolocal -v $(pwd):/maven_work_dir --name mvn collonvtom/maven:latest
